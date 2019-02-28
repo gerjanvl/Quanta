e5b51676ad17582784c4adfa96ddbc14e360242a
Gerjan vlot <gerjanvlot@live.nl>
2019-02-28 20:40:46 +0100

    Cleanup mappings

--- Quanta.WebApi/MappingProfiles/DomainProfile.cs
+++ Quanta.WebApi/MappingProfiles/DomainProfile.cs
@@ -8,12 +8,21 @@ namespace Quanta.WebApi.MappingProfiles
     {
         public DomainProfile()
         {
+            CreateUserMappings();
+            CreateDeviceMappings();
+        }
+
+        private void CreateUserMappings()
+        {
             CreateMap<DataAccess.Models.User, OData.Models.User>()
                 .ForMember(o => o.Devices, o => o.MapFrom(p => p.UserDevices.Select(ud => ud.Device)));
 
             CreateMap<OData.Models.User, DataAccess.Models.User>()
                 .ForMember(o => o.UserDevices, p => p.Ignore());
+        }
 
+        private void CreateDeviceMappings()
+        {
             CreateMap<Device, OData.Models.Device>();
 
             CreateMap<OData.Models.Device, Device>();
